1c5681e7615e4b40efc211489af59596e4a60f35
LuccaHaddadSerejo <hslucca@gmail.com>
2023-06-20 09:47:49 -0300

    feat: changing per page of repositories

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -4,12 +4,12 @@ import {
   ProjectStackTech,
   ProjectLink,
   ProjectLinks,
-} from './style';
+} from "./style";
 
-import { Text } from '@/styles/Text';
-import { useEffect, useState } from 'react';
-import { FaGithub, FaShare } from 'react-icons/fa';
-import { userData } from '@/utils/userData';
+import { Text } from "@/styles/Text";
+import { useEffect, useState } from "react";
+import { FaGithub, FaShare } from "react-icons/fa";
+import { userData } from "@/utils/userData";
 
 interface ReposType {
   html_url: string;
@@ -27,10 +27,12 @@ export const Project = (): JSX.Element => {
   useEffect(() => {
     const fetchData = async () => {
       const data: Response = await fetch(
-        `https://api.github.com/users/${userData.githubUser}/repos`
+        `https://api.github.com/users/${userData.githubUser}/repos?per_page=100`
       );
 
       const json = await data.json();
+
+      console.log(json);
 
       setRepositories(json);
 
@@ -48,33 +50,33 @@ export const Project = (): JSX.Element => {
       {repositories?.map((repository) => (
         <ProjectWrapper key={repository.id}>
           <Text
-            as='h2'
-            type='heading3'
-            css={{ marginBottom: '$3' }}
-            color='grey1'>
+            as="h2"
+            type="heading3"
+            css={{ marginBottom: "$3" }}
+            color="grey1">
             {repository.name}
           </Text>
 
           {repository.language && (
             <ProjectStack>
-              <Text type='body2'>Linguagem:</Text>
+              <Text type="body2">Linguagem:</Text>
               <ProjectStackTech>
-                <Text color='brand1' type='body2'>
+                <Text color="brand1" type="body2">
                   {repository.language}
                 </Text>
               </ProjectStackTech>
             </ProjectStack>
           )}
 
-          <Text type='body1' color='grey2'>
+          <Text type="body1" color="grey2">
             {repository.description}
           </Text>
           <ProjectLinks>
-            <ProjectLink target='_blank' href={repository.html_url}>
+            <ProjectLink target="_blank" href={repository.html_url}>
               <FaGithub /> Github Code
             </ProjectLink>
             {repository.homepage && (
-              <ProjectLink target='_blank' href={repository.homepage}>
+              <ProjectLink target="_blank" href={repository.homepage}>
                 <FaShare /> Aplicação
               </ProjectLink>
             )}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,15 @@
 // Styles
-import { Container } from '@/styles/Global';
-import { Text } from '@/styles/Text';
-import { Button } from '@/styles/Buttons';
+import { Container } from "@/styles/Global";
+import { Text } from "@/styles/Text";
+import { Button } from "@/styles/Buttons";
 
 // Components
-import { Stack } from '@/components/Stack';
-import { Project } from '@/components/Project';
+import { Stack } from "@/components/Stack";
+import { Project } from "@/components/Project";
 
 // Data
-import { stackData } from '@/utils/stackData';
-import { userData } from '@/utils/userData';
+import { stackData } from "@/utils/stackData";
+import { userData } from "@/utils/userData";
 
 // Page Style
 import {
@@ -24,7 +24,7 @@ import {
   ProjectAreaWrapperColumns,
   AboutMeSection,
   AboutMeContent,
-} from './style';
+} from "./style";
 
 export const Home = (): JSX.Element => {
   return (
@@ -32,28 +32,28 @@ export const Home = (): JSX.Element => {
       <Header>
         <Container>
           <HeaderContent>
-            <Text as='h1' type='heading1' color='grey5'>
-              Criando experiências por meio da tecnologia{' '}
+            <Text as="h1" type="heading1" color="grey5">
+              Criando experiências por meio da tecnologia{" "}
             </Text>
-            <Text type='body1' color='grey5'>
+            <Text type="body1" color="grey5">
               Sou estudante de programação na Kenzie Academy Brasil, participei
               de diversos projetos resolvendo problemas de alto nível e
               desenvolvendo habilidades.
             </Text>
             <HeaderButtonsArea>
-              <Button as='a' href='#projetos'>
+              <Button as="a" href="#projetos">
                 Projetos
               </Button>
-              <Button as='a' href='#tecnologias' type='btLink' color='grey5'>
+              <Button as="a" href="#tecnologias" type="btLink" color="grey5">
                 Tecnologias
               </Button>
             </HeaderButtonsArea>
           </HeaderContent>
         </Container>
       </Header>
-      <StackSection id='tecnologias'>
+      <StackSection id="tecnologias">
         <Container>
-          <Text as='h4' type='heading3' color='grey1'>
+          <Text as="h4" type="heading3" color="grey1">
             Ferramentas que domino
           </Text>
           <StackCards>
@@ -66,20 +66,20 @@ export const Home = (): JSX.Element => {
       <AboutMeSection>
         <Container>
           <AboutMeContent>
-            <Text as='h1' type='heading1' color='grey5'>
+            <Text as="h1" type="heading1" color="grey5">
               Sobre mim
             </Text>
-            <Text type='body1' color='grey5'>
+            <Text type="body1" color="grey5">
               Meu nome é Lucca Haddad Serejo, tenho 24 anos e moro em
               Brasília-DF. Comecei na programação e na área da tecnologia após
               alguns anos estudando Direito.
             </Text>
-            <Text type='body1' color='grey5'>
-              Estudo programação a pouco menos de um ano, tive experiência
+            <Text type="body1" color="grey5">
+              Estudo programação há pouco menos de um ano, tive experiência
               trabalhando na área tech como monitor de ensino na Kenzie Academy
               Brasil.
             </Text>
-            <Text type='body1' color='grey5'>
+            <Text type="body1" color="grey5">
               Atualmente estou finalizando meus estudos no curso de
               Desenvolvedor Web Full-Stack na Kenzie e me formando na faculdade
               de Análise e Desenvolvimento de Sistemas, sempre em busca de
@@ -88,38 +88,38 @@ export const Home = (): JSX.Element => {
           </AboutMeContent>
         </Container>
       </AboutMeSection>
-      <ProjectsArea id='projetos'>
+      <ProjectsArea id="projetos">
         <Container>
           <ProjectAreaWrapperColumns>
             <ProjectsAreaSocialMediaMessage>
-              <Text as='h2' type='heading4' color='grey1'>
+              <Text as="h2" type="heading4" color="grey1">
                 Vamos trocar uma ideia?
               </Text>
-              <Text as='p' type='body1' color='grey2'>
+              <Text as="p" type="body1" color="grey2">
                 No linkedIn sempre estou compartilhando meus processos diários
                 para desenvolver esses projetos e estou disposto a trocar
                 algumas ideias por lá.
               </Text>
               <Button
-                type='primary'
-                target='_blank'
-                as='a'
+                type="primary"
+                target="_blank"
+                as="a"
                 href={`https://www.linkedin.com/in/${userData.linkedinUser}`}>
                 Acessar perfil no LinkedIn
               </Button>
             </ProjectsAreaSocialMediaMessage>
             <ProjectsAreaContent>
               <Text
-                as='h2'
-                type='heading4'
-                color='grey1'
-                css={{ marginBottom: '$2' }}>
+                as="h2"
+                type="heading4"
+                color="grey1"
+                css={{ marginBottom: "$2" }}>
                 Projetos
               </Text>
-              <Text as='h3' type='heading2' color='grey1'>
-                <Text as='span' color='brand1' type='heading2'>
+              <Text as="h3" type="heading2" color="grey1">
+                <Text as="span" color="brand1" type="heading2">
                   Dedicação
-                </Text>{' '}
+                </Text>{" "}
                 em cada detalhe
               </Text>
               <Project />
