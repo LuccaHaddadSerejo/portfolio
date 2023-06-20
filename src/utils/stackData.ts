e2d7489289b5d25369753e1abdf9215be9045483
LuccaHaddadSerejo <hslucca@gmail.com>
2023-06-20 10:09:34 -0300

    feat: adding new technologies

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,29 +1,39 @@
-import { SiCss3 } from 'react-icons/si';
-import { SiTailwindcss } from 'react-icons/si';
-import { SiDjango } from 'react-icons/si';
-import { SiExpress } from 'react-icons/si';
-import { SiStyledcomponents } from 'react-icons/si';
-import { SiHtml5 } from 'react-icons/si';
-import { SiPostgresql } from 'react-icons/si';
-import { SiJavascript } from 'react-icons/si';
-import { SiTypescript } from 'react-icons/si';
-import { FaSass } from 'react-icons/fa';
-import { FaReact } from 'react-icons/fa';
-import { FaPython } from 'react-icons/fa';
-import { FaNodeJs } from 'react-icons/fa';
+import { SiCss3 } from "react-icons/si";
+import { SiTailwindcss } from "react-icons/si";
+import { SiDjango } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { SiStyledcomponents } from "react-icons/si";
+import { SiHtml5 } from "react-icons/si";
+import { SiPostgresql } from "react-icons/si";
+import { SiJavascript } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { SiNextdotjs } from "react-icons/si";
+import { SiNestjs } from "react-icons/si";
+import { SiJest } from "react-icons/si";
+import { SiFastapi } from "react-icons/si";
+import { FaSass } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { FaDocker } from "react-icons/fa";
 
 export const stackData = [
-  { title: 'TypeScript', img: SiTypescript },
-  { title: 'React', img: FaReact },
-  { title: 'Node.js', img: FaNodeJs },
-  { title: 'Express.js', img: SiExpress },
-  { title: 'Python', img: FaPython },
-  { title: 'Django', img: SiDjango },
-  { title: 'PostgreSQL', img: SiPostgresql },
-  { title: 'JavaScript', img: SiJavascript },
-  { title: 'CSS', img: SiCss3 },
-  { title: 'Styled-Components', img: SiStyledcomponents },
-  { title: 'TailWind', img: SiTailwindcss },
-  { title: 'Sass', img: FaSass },
-  { title: 'HTML', img: SiHtml5 },
+  { title: "TypeScript", img: SiTypescript },
+  { title: "React.js", img: FaReact },
+  { title: "Next.js", img: SiNextdotjs },
+  { title: "Node.js", img: FaNodeJs },
+  { title: "Express.js", img: SiExpress },
+  { title: "NestJS", img: SiNestjs },
+  { title: "Jest", img: SiJest },
+  { title: "Python", img: FaPython },
+  { title: "Django", img: SiDjango },
+  { title: "FastAPI", img: SiFastapi },
+  { title: "PostgreSQL", img: SiPostgresql },
+  { title: "JavaScript", img: SiJavascript },
+  { title: "Docker", img: FaDocker },
+  { title: "CSS", img: SiCss3 },
+  { title: "Styled-Components", img: SiStyledcomponents },
+  { title: "TailWind", img: SiTailwindcss },
+  { title: "Sass", img: FaSass },
+  { title: "HTML", img: SiHtml5 },
 ];
